0ce5796e992c4b3df9ee03ff770f6996b4d4ff73
unknown <sademark08@gmail.com>
2019-12-17 10:24:07 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <title>Todo List</title>
+</head>
+<body>
+    @yield("content")
+</body>
+</html>
